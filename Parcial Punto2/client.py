0c8235230c65f3c09f49d080f2fc8c84d683fa9a
estebanstack <130319203+estebanstack@users.noreply.github.com>
2025-03-11 20:33:36 -0500

    Update client.py

--- Parcial Punto2/client.py
+++ Parcial Punto2/client.py
@@ -1,17 +1,19 @@
 import socket
 
 def start_client():
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #-> crea el socket
     try:
         client.connect(('127.0.0.1', 2010))
         print("Conectado al servidor en localhost:2010")
         
         message = input('Ingrese el mensaje a enviar: ')
         print(f"Enviando mensaje: {message}")
-        client.sendall(message.encode('utf-8'))
+        client.sendall(message.encode('utf-8')) #->  Convierte el mensaje en bytes usando codificación UTF-8 y lo envía al servidor.
+        # sendall solo recibe bytes
         
+        # Espera recibir una respuesta del servidor (hasta 1024 bytes) y la decodifica de UTF-8 a string.
         response = client.recv(1024).decode('utf-8')
-        if response:
+        if response: 
             print(f"Respuesta del servidor: {response}")
         else:
             print("No se recibió respuesta del servidor.")
